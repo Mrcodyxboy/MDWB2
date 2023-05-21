67d184cd711bf8716c8d51bb277b89950be26995
Mrcodyxboy <134197970+Mrcodyxboy@users.noreply.github.com>
2023-05-21 16:14:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from configs import Config
 from pyrogram import Client, idle
 import asyncio
 from telethon import TelegramClient
+
 from telethon.sessions import StringSession
 from plugins.tgraph import *
 from helpers import *
@@ -13,7 +14,7 @@ from telethon import TelegramClient, events
 import urllib.parse
 from telethon.errors import UserNotParticipantError
 from telethon.tl.functions.channels import GetParticipantRequest
-
+import re
 tbot = TelegramClient('mdisktelethonbot', Config.API_ID, Config.API_HASH).start(bot_token=Config.BOT_TOKEN)
 client = TelegramClient(StringSession( Config.USER_SESSION_STRING), Config.API_ID, Config.API_HASH)
 
@@ -54,7 +55,6 @@ async def ping_server():
         except Exception:
             traceback.print_exc()
 
-
 async def get_user_join(id):
     if Config.FORCE_SUB == "False":
         return True
@@ -70,44 +70,56 @@ async def get_user_join(id):
 
 @tbot.on(events.NewMessage(incoming=True))
 async def message_handler(event):
+    try:
+        if event.message.post:
+            return
 
-    if event.message.post:
-        return
+        # if event.is_channel:return
+        if event.text.startswith("/"):return
 
-    print("\n")
-    print("Message Received: " + event.text)
-    # if event.is_channel:return
-    if event.text.startswith("/"):return
+        print("\n")
+        print("Message Received: " + event.text)
 
-    # Force Subscription
-    if  not await get_user_join(event.sender_id):
-        haha = await event.reply(f'''**Hey! {event.sender.first_name} 😃**
+        # Force Subscription
+        if  not await get_user_join(event.sender_id):
+            haha = await event.reply(f'''**Hey! {event.sender.first_name} 😃**
 
 **You Have To Join Our Update Channel To Use Me ✅**
 
 **Click Bellow Button To Join Now.👇🏻**''', buttons=Button.url('🍿Updates Channel🍿', f'https://t.me/{Config.UPDATES_CHANNEL_USERNAME}'))
-        await asyncio.sleep(Config.AUTO_DELETE_TIME)
-        return await haha.delete()
+            await asyncio.sleep(Config.AUTO_DELETE_TIME)
+            return await haha.delete()
 
-    
-    print("Group: " + str(event.is_group))
-    print("Channel: " + str(event.is_channel))
-    args = event.text
-    args = await validate_q(args)
+        args = event.text
+        args = await validate_q(args)
 
-    print("Search Query: {args}".format(args=args))
-    print("\n")
-    
-    if not args:
-        return
+        print("Search Query: {args}".format(args=args))
+        print("\n")
 
-    txt = await event.reply('**Searching For "{}" 🔍**'.format(event.text))
+        if not args:
+            return
 
-    try:
+        txt = await event.reply('**Searching For "{}" 🔍**'.format(event.text))
+
+
+
         search = []
-        async for i in AsyncIter(args.split()):
-            search_msg = client.iter_messages(Config.CHANNEL_ID, limit=5, search=i)
-            search.append(search_msg)
+        if event.is_group or event.is_channel:
+            group_info = await db.get_group(str(event.chat_id).replace("-100", ""))
+
+            if group_info["has_access"] and group_info["db_channel"] and await db.is_group_verified(str(event.chat_id).replace("-100", "")):
+                CHANNEL_ID = group_info["db_channel"]
+            else:
+                CHANNEL_ID = Config.CHANNEL_ID
+        else:
+            CHANNEL_ID = Config.CHANNEL_ID
+
+
+        async for i in AsyncIter(re.sub("__|\*", "", args).split()):
+            if len(i) > 2:
+               
+                search_msg = client.iter_messages(CHANNEL_ID, limit=5, search=i)
+                search.append(search_msg)
 
         username = Config.UPDATES_CHANNEL_USERNAME
         answer = f'**Join** [@{username}](https://telegram.me/{username}) \n\n'
@@ -117,14 +129,11 @@ async def message_handler(event):
         async for msg_list in AsyncIter(search):
             async for msg in msg_list:
                 c += 1
-                f_text = msg.text.replace("*", "")
-
-  #              if event.is_group or event.is_channel:
-  #                 f_text = await group_link_convertor(event.chat_id, f_text)
+                f_text = re.sub("__|\*", "", msg.text)
 
                 f_text = await link_to_hyperlink(f_text)
-                answer += f'\n\n**✅ PAGE {c}:**\n\n━━━━━━━━━\n\n' + '' + f_text.split("\n", 1)[0] + '' + '\n\n' + '' + f_text.split("\n", 2)[
-                    -1] 
+                answer += f'\n\n\n✅ PAGE {c}:\n\n━━━━━━━━━\n\n' + '' + f_text.split("\n", 1)[0] + '' + '\n\n' + '' + f_text.split("\n", 2)[
+                    -1] + "\n\n"
                 
             # break
         finalsearch = []
@@ -136,14 +145,12 @@ async def message_handler(event):
 
 **Type Only Movie Name 💬**
 **Check Spelling On** [Google](http://www.google.com/search?q={event.text.replace(' ', '%20')}%20Movie) 🔍
-'''
+    '''
 
             newbutton = [Button.url('Click To Check Spelling ✅',
                                     f'http://www.google.com/search?q={event.text.replace(" ", "%20")}%20Movie')], [
                             Button.url('Click To Check Release Date 📅',
-                                    f'http://www.google.com/search?q={event.text.replace(" ", "%20")}%20Movie%20Release%20Date')], [
-                            Button.url('👉 Request Your Movie 👈',
-                                    f'https://instagram.com/royalkrrishna?igshid=YmMyMTA2M2Y=')]
+                                    f'http://www.google.com/search?q={event.text.replace(" ", "%20")}%20Movie%20Release%20Date')]
             await txt.delete()
             result = await event.reply(answer, buttons=newbutton, link_preview=False)
             await asyncio.sleep(Config.AUTO_DELETE_TIME)
@@ -156,27 +163,24 @@ async def message_handler(event):
         answer = await replace_username(answer)
         html_content = await markdown_to_html(answer)
         html_content = await make_bold(html_content)
+        
         tgraph_result = await telegraph_handler(
             html=html_content,
             title=event.text,
             author=Config.BOT_USERNAME
         )
         message = f'**Click Here 👇 For "{event.text}"**\n\n[🍿🎬 {str(event.text).upper()}\n🍿🎬 {str("Click me for results").upper()}]({tgraph_result})'
-        button =  [Button.url('❓How To Open Link❓',
-                                    f'https://youtu.be/5H61FQ6ifWc')], [
-                            Button.url('👉 Request Your Movie 👈',
-                                    f'https://instagram.com/royalkrrishna?igshid=YmMyMTA2M2Y=')]
 
         await txt.delete()
-        result = await event.reply(message, buttons=button, link_preview=False)
+        result = await event.reply(message, link_preview=False)
         await asyncio.sleep(Config.AUTO_DELETE_TIME)
-        await event.delete()
+        # await event.delete()
         return await result.delete()
 
     except Exception as e:
         print(e)
         await txt.delete()
-        result = await event.reply("**Some Error While Searching...‼️\n\nReport @RoyalKrrishn 🥷**")
+        result = await event.reply("Please Search Again...🔍🙏")
         await asyncio.sleep(Config.AUTO_DELETE_TIME)
         await event.delete() 
         return await result.delete()
@@ -240,3 +244,7 @@ print()
 print("------------------------ Stopped Services ------------------------")
 Bot.stop()
 # User.stop()
+
+
+
+
